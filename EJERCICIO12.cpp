fb1d48c95aec61f4d8eba20e775d971452afad0d
Jose <jvilcalqui@unjbg.edu.pe>
2024-06-13 23:22:46 -0500

    Modificacion del mensaje al usuario

--- EJERCICIO12.cpp
+++ EJERCICIO12.cpp
@@ -1,10 +1,11 @@
 /*EJERCICIO 12: Calcular e imprimir la tabla de multiplicar de un número cualquiera. Imprimir el
 multiplicando, el multiplicador y el producto.*/
+
 #include <iostream>
 using namespace std;
 int main(){
 	int n,i,p;
-	cout<<"DE EL NUMERO: "<<endl;
+	cout<<"Ingrese el numero: "<<endl;
 	cin>>n;
 	i=1;
 	do{
