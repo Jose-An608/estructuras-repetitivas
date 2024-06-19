bfa885261da8d7539dbaa259cfb71d28de4fc46b
Jose <jvilcanquic@unjbg.edu.pe>
2024-06-18 23:26:00 -0500

    Se cambio el bucle while por el for

--- EJERCICIO1.cpp
+++ EJERCICIO1.cpp
@@ -5,11 +5,13 @@ int main(){
 	int n, i, m7;
 	cout <<" Ingresa la cantidad de numeros multiplos de 7: ";
 	cin >> n;
-	i = 1;
-	while (i<=n){
-		m7 = i * 7;
+	
+	cout<<" Los multiplos de 7 son: "<<endl;
+	
+	for(i=1 ; i<=n ; i++){
+		
+		m7= i*7;
 		cout << m7 << endl;
-		i++;	
 	}
 	return 0;
 }
